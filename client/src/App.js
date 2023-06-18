6d7ad1ad7ef799264cb0c98195bb5cfdf84c69a8
David He <davidinmy@qq.com>
2023-06-18 17:14:18 +0800

    Add Not found page

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 import PrivateRoute from "./components/routing/PrivateRoute";
 //Redux
 import { Provider } from "react-redux";
@@ -68,6 +69,7 @@ const App = () => {
             path="/posts/:id"
             element={<PrivateRoute component={Post} />}
           />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </Provider>
